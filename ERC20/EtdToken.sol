7073d548fc3e5188dc29f13d4f3cf06a2793a9cf
zpykillcc <929784745@qq.com>
2022-01-27 11:18:35 +0800

    Update EtdToken.sol

--- ERC20/EtdToken.sol
+++ ERC20/EtdToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.16;
 
 interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-contract TokenERC20 {
+contract tokenERC20 {
     string public name;
     string public symbol;
     uint8 public decimals = 18;  // 18 是建议的默认值
